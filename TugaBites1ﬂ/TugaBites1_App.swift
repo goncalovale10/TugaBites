474770baab64d70cbd5ebda4e8f97187dfc6e1e5
Gonçalo <1220695@isep.ipp.pt>
2025-11-28 11:03:49 +0100

    Initial Commit

---
+++ TugaBites1ﬂ/TugaBites1_App.swift
@@ -0,0 +1,17 @@
+//
+//  TugaBites1_App.swift
+//  TugaBites1ﬂ
+//
+//  Created by Mariana Rocha on 28/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct TugaBites1_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
